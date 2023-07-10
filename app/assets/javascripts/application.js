3ace73573d6c74e52671a4ac0cffbcd0756d613c
Jakub <jakub.dobrowolski@pwc.com>
2023-07-10 16:16:36 +0200

    Add acvtive links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,16 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+  let all_links = document.querySelector('.govuk-header__navigation').querySelectorAll(".govuk-header__link"),
+      
+        full_path = location.href.split('#')[0]; //Ignore hashes?
+
+
+    all_links.forEach(link => {
+      if(full_path.includes(link.href.split("#"))  && !link.className.includes("hide")) {
+        link.parentElement.className+=" govuk-header__navigation-item--active"
+        console.log('full: ' + full_path, 'link: ' + link.href.split("#"))
+
+      }
+    })
 })
